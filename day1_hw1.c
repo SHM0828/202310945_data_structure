358bc0060f06aaac2bec619a180f0db6fa1add4b
SHM0828 <127410040+SHM0828@users.noreply.github.com>
2024-03-12 15:22:03 +0900

    unicode error fixed in day1_hw1.c

--- day1_hw1.c
+++ day1_hw1.c
@@ -1,9 +1,8 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main() {
-	char* ptr1[4] = {"Korea", "Seoul", "Mapo", "152¹øÁö 2/3"};
-	char* ptr2[4] = { "Korea", "Seoul", "Jongno", "152¹øÁö 2/3" };
+	char* ptr1[4] = {"Korea", "Seoul", "Mapo", "152ë²ˆì§€ 2/3"};
+	char* ptr2[4] = { "Korea", "Seoul", "Jongno", "152ë²ˆì§€ 2/3" };
 	for (int i = 0; i < 4; i++) {
 		printf("%s\n", ptr1[i]);
 	}
